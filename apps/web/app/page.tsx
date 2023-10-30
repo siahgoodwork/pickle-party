d5a929304b8548666f4e39064ca734e047ed7ca7
Siah <siah@goodwork.sg>
2023-10-30 19:21:41 +0800

    feat: frontend mockup vibes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -68,60 +68,81 @@ export default function Page(): JSX.Element {
           </button>
         </div>
       ) : (
-        <div className="p-4">
-          <h1>Hi {userId}</h1>
-          {activePoll !== undefined ? (
-            <div className="">
-              <div>Poll</div>
-              <h3 className="py-8 text-lg">{activePoll.question}</h3>
-              {activePollResults && userVotedActivePoll ? (
-                <div>
-                  {activePollResults.choices.map((c) => (
-                    <div key={c.id}>
-                      {activePoll.choices.find((cp) => cp.id === c.id)?.text} -{" "}
-                      {c.voters.length}
-                    </div>
-                  ))}
-                </div>
-              ) : (
-                <div className="flex flex-col w-60 gap-2">
-                  {activePoll.choices.map((choice) => (
-                    <button
-                      type="button"
-                      key={choice.id}
-                      onClick={() => {
-                        const currentResultIndex = state.pollResults.findIndex(
-                          (pr) => pr.id === activePoll.id
-                        );
-                        if (currentResultIndex < 0) {
-                          state.pollResults.push({
-                            id: activePoll.id,
-                            choices: activePoll.choices.map((c) => ({
-                              voters: choice.id === c.id ? [userId] : [],
-                              id: c.id,
-                            })),
-                          });
-                        } else {
-                          const choiceIndex = state.pollResults[
-                            currentResultIndex
-                          ].choices.findIndex((c) => c.id === choice.id);
-                          if (choiceIndex > -1) {
-                            state.pollResults[currentResultIndex].choices[
-                              choiceIndex
-                            ].voters.push(userId);
+        <div className="p-4 grid-cols-4 grid gap-4 grid-rows-4">
+          <div className="col-span-3 aspect-[16/9] row-span-4">
+            <iframe
+              className="w-full h-full"
+              src="https://www.youtube.com/embed/f_7QbCTkTOI"
+              title="YouTube video player"
+            />
+          </div>
+          <div className="col-span-1 row-span-2">
+            {activePoll !== undefined ? (
+              <div className="w-full h-full p-2 border border-black">
+                <div>Poll</div>
+                <h3 className="py-8 text-lg">{activePoll.question}</h3>
+                {activePollResults && userVotedActivePoll ? (
+                  <div>
+                    {activePollResults.choices.map((c, _n, allChoices) => (
+                      <div key={c.id}>
+                        {activePoll.choices.find((cp) => cp.id === c.id)?.text}{" "}
+                        -{" "}
+                        {(
+                          (c.voters.length /
+                            allChoices.reduce(
+                              (p, __c) => p + __c.voters.length,
+                              0
+                            )) *
+                          100
+                        ).toFixed(2)}{" "}
+                        %
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <div className="flex flex-col w-60 gap-2">
+                    {activePoll.choices.map((choice) => (
+                      <button
+                        type="button"
+                        key={choice.id}
+                        onClick={() => {
+                          const currentResultIndex =
+                            state.pollResults.findIndex(
+                              (pr) => pr.id === activePoll.id
+                            );
+                          if (currentResultIndex < 0) {
+                            state.pollResults.push({
+                              id: activePoll.id,
+                              choices: activePoll.choices.map((c) => ({
+                                voters: choice.id === c.id ? [userId] : [],
+                                id: c.id,
+                              })),
+                            });
+                          } else {
+                            const choiceIndex = state.pollResults[
+                              currentResultIndex
+                            ].choices.findIndex((c) => c.id === choice.id);
+                            if (choiceIndex > -1) {
+                              state.pollResults[currentResultIndex].choices[
+                                choiceIndex
+                              ].voters.push(userId);
+                            }
                           }
-                        }
-                      }}
-                    >
-                      {choice.text}
-                    </button>
-                  ))}
-                </div>
-              )}
-            </div>
-          ) : (
-            <div>No poll</div>
-          )}
+                        }}
+                      >
+                        {choice.text}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div>No poll</div>
+            )}
+          </div>
+          <div className="p-2 border border-black row-span-2">
+            Chat goes here
+          </div>
         </div>
       )}
     </main>
