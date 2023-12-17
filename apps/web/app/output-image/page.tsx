fd8940735cce4915e2058b1787c1e455a1c8de94
Siah <siah@goodwork.sg>
2023-12-17 13:16:14 +0800

    feat: manually refresh state to make sure page rerenders

--- apps/web/app/output-image/page.tsx
+++ apps/web/app/output-image/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { useSyncedStore } from "@syncedstore/react";
+import { useEffect, useState } from "react";
 import { store } from "../store";
 
 export default function Ticker(): React.ReactNode {
   const { chatOutput } = useSyncedStore(store);
 
-  //eslint-disable-next-line -- debugging
-  console.log(chatOutput.imageUrl);
+  const [yesno, setYesno] = useState(false);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setYesno((a) => !a);
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
 
   if (chatOutput.imageUrl === undefined) {
     return <div className="fixed top-0 left-0 w-full h-full">No image</div>;
@@ -20,6 +29,8 @@ export default function Ticker(): React.ReactNode {
         alt="chat output"
         className="absolute object-cover w-full h-full"
       />
+
+      <span className="h-[0]">{yesno ? " " : "  "}</span>
     </div>
   );
 }
--- apps/web/app/output-text/page.tsx
+++ apps/web/app/output-text/page.tsx
@@ -1,10 +1,21 @@
 "use client";
 
 import { useSyncedStore } from "@syncedstore/react";
+import { useEffect, useState } from "react";
 import { store } from "../store";
 
 export default function Ticker(): React.ReactNode {
   const { chatOutput } = useSyncedStore(store);
+  const [yesno, setYesno] = useState(false);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setYesno((a) => !a);
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
 
   return (
     <div
@@ -20,6 +31,8 @@ export default function Ticker(): React.ReactNode {
       <p className="w-[80vw] text-[2vw] whitespace-pre-line font-vcr leading-[1.1] text-center">
         {chatOutput.body}
       </p>
+
+      <span className="h-[0]">{yesno ? " " : "  "}</span>
     </div>
   );
 }
