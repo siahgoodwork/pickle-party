e58432de790a8e89443d2c0a3663e34ec7f189d3
Siah <siah@goodwork.sg>
2023-12-17 10:17:24 +0800

    chore: debug chatOutput

--- apps/web/app/output-image/page.tsx
+++ apps/web/app/output-image/page.tsx
@@ -6,6 +6,9 @@ import { store } from "../store";
 export default function Ticker(): React.ReactNode {
   const { chatOutput } = useSyncedStore(store);
 
+  //eslint-disable-next-line -- debugging
+  console.log(chatOutput);
+
   if (chatOutput.imageUrl === undefined) {
     return <div className="fixed top-0 left-0 w-full h-full">No image</div>;
   }
